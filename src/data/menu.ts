d49f07acbd9cead2925c1820a1846afc1b932f95
Murtazo1979 <murtazo1979@gmail.com>
2025-09-23 18:43:32 +0500

    Update menu.ts

--- src/data/menu.ts
+++ src/data/menu.ts
@@ -1,1 +1,7 @@
-
+// src/data/menu.ts
+export const menuItems = [
+  { label: "Home", path: "/" },
+  { label: "About", path: "/about" },
+  { label: "Contact", path: "/contact" },
+  { label: "Privacy", path: "/privacy" },
+];
