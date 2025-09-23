59e8d3c43b4fc714063d87dafed799893a6f3ea3
Murtazo1979 <murtazo1979@gmail.com>
2025-09-23 19:34:13 +0500

    Update MainLayout.tsx

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,1 +1,23 @@
+// src/layouts/MainLayout.tsx
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
+interface MainLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function MainLayout({ children }: MainLayoutProps) {
+  return (
+    <div className="flex flex-col min-h-screen bg-gray-50 dark:bg-gray-900">
+      {/* Navbar */}
+      <Navbar />
+
+      {/* Page Content */}
+      <main className="flex-grow container mx-auto px-4 py-6">{children}</main>
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
+}
