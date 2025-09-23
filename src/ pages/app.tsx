25b607450b4fc6f52246a77a1740f7ad199c39c9
Murtazo1979 <murtazo1979@gmail.com>
2025-09-23 18:30:47 +0500

    Update app.tsx

--- src/ pages/app.tsx
+++ src/ pages/app.tsx
@@ -1,11 +1,17 @@
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
+import { AuthProvider } from "../context/AuthContext";
+import { ThemeProvider } from "../context/ThemeContext";
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <AuthProvider>
+      <ThemeProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </AuthProvider>
   );
 }
