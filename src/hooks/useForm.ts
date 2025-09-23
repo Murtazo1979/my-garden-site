99acc1e394519defeb4b1bf3f04cae78121eb79b
Murtazo1979 <murtazo1979@gmail.com>
2025-09-23 19:07:35 +0500

    Update useForm.ts

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,1 +1,15 @@
+// src/hooks/useForm.ts
+import { useState } from "react";
 
+export function useForm<T extends Record<string, any>>(initialValues: T) {
+  const [values, setValues] = useState<T>(initialValues);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  const resetForm = () => setValues(initialValues);
+
+  return { values, handleChange, resetForm };
+}
