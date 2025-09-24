2a15e454e58a6f486161ff586320ff7038317a61
Murtazo1979 <murtazo1979@gmail.com>
2025-09-24 07:22:35 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,20 @@
+---
 
+### 📂 next.config.js
+`js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["images.unsplash.com", "picsum.photos"], // External images
+  },
+  i18n: {
+    locales: ["en", "uz", "ru"],
+    defaultLocale: "en",
+  },
+  compiler: {
+    styledComponents: true,
+  },
+};
+
+module.exports = nextConfig;
