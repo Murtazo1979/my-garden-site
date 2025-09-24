bc8aa5631a3482d3a2887b106795a38af697371d
Murtazo1979 <murtazo1979@gmail.com>
2025-09-24 07:17:03 +0500

    Update Navbar.test.tsx

--- src/tests/Navbar.test.tsx
+++ src/tests/Navbar.test.tsx
@@ -1,1 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import Navbar from "../components/Navbar";
+import { ThemeProvider } from "../context/ThemeContext";
 
+describe("Navbar Component", () => {
+  test("renders site logo", () => {
+    render(
+      <ThemeProvider>
+        <Navbar />
+      </ThemeProvider>
+    );
+    expect(screen.getByText(/MyGarden/i)).toBeInTheDocument();
+  });
+
+  test("renders menu items", () => {
+    render(
+      <ThemeProvider>
+        <Navbar />
+      </ThemeProvider>
+    );
+    const menuItems = ["Home", "About", "Contact"]; // menu.ts да бор номлар
+    menuItems.forEach((item) => {
+      expect(screen.getByText(item)).toBeInTheDocument();
+    });
+  });
+});
